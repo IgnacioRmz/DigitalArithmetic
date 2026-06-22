8d0673a5cdf0ea65f3bad3bb0be2357fec9bbea5
Ramirez Padilla, Luis I <ignacio.ramirez@solidigm.com>
2026-06-22 15:58:16 -0600

    Add optimized fma, behavioral fma and mult/add separated fma

---
+++ Src/fma_behavioral.sv
@@ -0,0 +1,29 @@
+module fma_behavioral #(
+    parameter  int SRC1_WIDTH   = 64,
+    parameter  int SRC2_WIDTH   = SRC1_WIDTH,
+    parameter  int SRC3_WIDTH   = SRC1_WIDTH,
+    localparam int RESULT_WIDTH = (SRC1_WIDTH + SRC2_WIDTH)
+) (
+    input  logic [SRC1_WIDTH-1:0]   srca,
+    input  logic [SRC2_WIDTH-1:0]   srcb,
+    input  logic [SRC3_WIDTH-1:0]   srcc,
+    input  logic                    is_fma,
+    input  logic                    is_signed,
+    output logic [RESULT_WIDTH-1:0] result
+);
+
+    always_comb begin
+        if (is_signed) begin
+            result = $signed(srca) * $signed(srcb);
+            if (is_fma) begin
+                result = $signed(result) + $signed(srcc);
+            end
+        end else begin
+            result = srca * srcb;
+            if (is_fma) begin
+                result = result + srcc;
+            end
+        end
+    end
+
+endmodule
