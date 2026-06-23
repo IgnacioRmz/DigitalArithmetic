62fdefaf9f4a201011f6aaafe618476ed58de319
Ignacio Ramirez <luisignacio_rmzp@hotmail.com>
2026-06-22 23:00:20 -0600

    remove localparam to make quartus compatible

--- Src/fma.sv
+++ Src/fma.sv
@@ -2,7 +2,7 @@ module fma #(
     parameter  int SRC1_WIDTH   = 64,
     parameter  int SRC2_WIDTH   = SRC1_WIDTH,
     parameter  int SRC3_WIDTH   = SRC1_WIDTH,
-    localparam int RESULT_WIDTH = (SRC1_WIDTH + SRC2_WIDTH)
+    parameter int RESULT_WIDTH = (SRC1_WIDTH + SRC2_WIDTH)
 ) (
     input  logic [SRC1_WIDTH-1:0]   srca,
     input  logic [SRC2_WIDTH-1:0]   srcb,
